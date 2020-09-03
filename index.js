bc4f94459ff158a1688fff13b2cf7c33c24b0770
foo <vinhphuctadang@gmail.com>
2020-09-03 23:08:41 +0700

    verify jwt

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const jwt = require('jsonwebtoken')
-
-async function main(){
-    
-    let token = jwt.sign(
-        { 
-            alias: 'tony',
-        }, 
+const 
+    rsaPriv = 
 `-----BEGIN RSA PRIVATE KEY-----
 MIIBOQIBAAJBAIy8nbdKGBgaDJSD1v86bsrCosGYTa6IaWN5O4iJ+kB1LMV1OazQ
 sbQwIAfRZMLV1T8lq4tHoKV4t5ADuDbCfAkCAwEAAQJATmlgbhioJ/WLgMub5AQI
@@ -14,16 +9,39 @@ AQIhAMj0ss+X0NEmtOLI0uIHVB49fkNK9U4M9RRYKbeYE95hAiEAs0lF85BOTfiK
 2ft54uvUQm7mOBrnw7UIPpc9rol3bqkCIHzpZbCWXK11UuDZFgR7q7T0YmSv1lVC
 xqBzwNKnTEphAiBUbXcBgb0+PoK0sBXv5HpqZmsHlLP7l26weuxTGu0hMQIgZ21y
 7iszqY/SP5Hrl46OuOXdouM9Hi6jtNgmI3aBjVY=
------END RSA PRIVATE KEY-----`
-    ,
-    {
-        algorithm: 'RS256'
-    })
+-----END RSA PRIVATE KEY-----`,
+    rsaPub = 
+`-----BEGIN PUBLIC KEY-----
+MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAIy8nbdKGBgaDJSD1v86bsrCosGYTa6I
+aWN5O4iJ+kB1LMV1OazQsbQwIAfRZMLV1T8lq4tHoKV4t5ADuDbCfAkCAwEAAQ==
+-----END PUBLIC KEY-----
+`
+    
+async function main(){
+    
+    // generate
+    let token = jwt.sign(
+        { 
+            alias: 'tony',
+        }, 
+        rsaPriv,
+        {
+            algorithm: 'RS256'
+        }
+    )
 
-    console.log({
-        token: token,
-        length: token.length
-    })
+    console.log(
+        'Generated jwt:',
+        {
+            token: token,
+            length: token.length
+        }
+        
+    )
+
+    // verify
+    var decoded = jwt.verify(token, rsaPub, {algorithms: 'RS256'})
+    console.log('Decoded jwt payload:', decoded)
 }
 
 main()
